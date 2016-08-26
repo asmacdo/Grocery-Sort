e76477cec2cef9a8b99ecbad7bc05c5bb2be7cd4
JayWelborn <jesse.welborn@gmail.com>
2016-08-26 20:21:10 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,75 +1,10 @@
 todays_list = []
 todays_sorted_list = []
-master_list = [x.strip('\n') for x in open("listsave.txt").readlines()]
 checked_list = []
-print "master_list is: ", master_list
+master_list = [x.strip('\n') for x in open("listsave.txt").readlines()]
 
 
-def fix_master_list():
-	
-	count = 0
-	position = 0
-	while count <= len(checked_list):
-		if checked_list[count] in master_list:
-			position = master_list.index(checked_list[count])
-			count += 1
-			return position
-		elif (checked_list[count] not in master_list) and (position != 0):
-			master_list.insert(position + 1, checked_list[count])
-			count +=1
-		elif (checked_list[count] not in master_list) and (position == 0):
-			master_list.insert(position, checked_list[count])
-		else:
-			print "There has been an unknown error"
-			
-	target = open("listsave.txt", "w")
-	target.write("\n".join(str(x) for x in master_list))
-	print master_list
-	quit()
-	
-##	This function sorts todays items in the order the user checks them off,
-##	and makes a new list, including new items from today.	
-	
-def check_items():
-
-	item = raw_input("Item checked: ")
-	
-	if item in todays_sorted_list:
-		todays_sorted_list.remove(item)
-		checked_list.append(item)
-		print item, "; check!"
-		check_items()
-		
-	else:
-		print "Item not on list"
-		
-	if len(todays_sorted_list) == 0:
-		print "Items were checked in this order: ", checked_list
-		fix_master_list()
-	else:
-		pass
-		
-	
-##	This function will compare items from today's list to the master list,
-##	and sort them accordingly. It will place items not on the master list at the end.	
-		
-def sort_todays_items():
-	
-	while 0 < len(todays_list):
-		if todays_list[0] in master_list:
-			position = master_list.index(todays_list[0])
-			todays_sorted_list.insert(position, todays_list.pop(0))
-			return todays_sorted_list
-		elif todays_list[0] not in master_list:
-			todays_sorted_list.append(todays_list.pop(0))
-			return todays_sorted_list
-		else:
-			print "Unknown Error."
-	print "Today's list is: ", todays_sorted_list
-	check_items()
-
-	
-##	This Function gets a list of items from the user and puts them into a list
+# #	This Function gets a list of items from the user and puts them into a list
 
 def get_items():
 
@@ -79,11 +14,75 @@ def get_items():
 		get_items()
 	elif 'finish' in item:
 		print "Today's list is: ", todays_list
-		return todays_list
 	else:
 		print "There has been an unknown error."
-	sort_todays_items()
 		
+		
+# #	This function will compare items from today's list to the master list,
+# #	and sort them accordingly. It will place items not on the master list at the end.	
+		
+def sort_todays_items():
+	
+	
+	for item in todays_list:
+		if item in master_list:
+			position = master_list.index(item)
+			todays_sorted_list.insert(position, item)
+			
+		elif item not in master_list:
+			todays_sorted_list.append(item)
+			
+		else:
+			print "Unknown Error."
+	
+	print "Today's list is: ", todays_sorted_list
+	
+	
+# #	This function sorts todays items in the order the user checks them off,
+# #	and makes a new list, including new items from today.	
+	
+def check_items():
+	
+	while len(todays_sorted_list) > 0:
+
+		item = raw_input("Item checked: ")
+		
+		if item in todays_sorted_list:
+			todays_sorted_list.remove(item)
+			checked_list.append(item)
+			print item, "; check!"
+			
+		elif item not in todays_sorted_list:
+			print "Item not on list"
+		
+	if len(todays_sorted_list) == 0:
+		print "Items were checked in this order: ", checked_list
+
+	else:
+		print "check_items error"
+
+
+## This function adds items checked today to the master list
+## If the items aren't already on the master list, it will add them
+## Immediately after the previously checked item
+
+def fix_master_list():
+	
+	count = 0
+	
+	for item in checked_list:
+		
+		if item in master_list:
+			count = master_list.index(item)
+			
+		elif item not in master_list and count == 0:
+			master_list.insert(count, item)
+			
+		elif item not in master_list and count != 0:
+			master_list.insert(count + 1, item)
+	
+	target = open("listsave.txt", 'w')		
+	target.writelines( "%s\n" % item for item in master_list )
 
 	
 get_items()
